30b073f3fb57a74b9f33a4fbe176ed4e43d61f77
Temirlan006 <ahunzanovtemirlan@gmail.com>
2022-03-05 19:49:02 +0600

    dz1

---
+++ lesson1/main.js
@@ -0,0 +1,28 @@
+const a = "20" //string or text
+const b = 20 // number
+
+let c = "Sanjar"
+
+const obj = {
+    key: "value",//клю ч и значение
+    obj2: {
+        dota: 77,
+        kostyl: "Jalil"
+    }
+}
+
+
+// console.log(typeof a)
+// console.log(obj.obj2.kostyl);
+
+const arr = ["Aidana", "Adinay", "Sanjar", "Jalil"]
+console.log(arr[3]);
+
+const first_number = prompt("первое число?")
+const last_number = prompt("второе число?")
+
+if (first_number > last_number){
+    console.log(first_number + ">" + last_number)
+};
+
+const otvet = alert("за домашку 10")
